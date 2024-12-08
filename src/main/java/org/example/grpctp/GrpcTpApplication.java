5977cec3c2632f257d9c3ce2116657487d364b8a
ABDR4FI3 <its.abdrafi3@gmail.com>
2024-12-08 10:49:09 +0100

    first commit

---
+++ src/main/java/org/example/grpctp/GrpcTpApplication.java
@@ -0,0 +1,13 @@
+package org.example.grpctp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcTpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcTpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/grpctp/GrpcTpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.grpctp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcTpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
